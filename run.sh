52d721a71cada13766380aeb08062d61647fdede
c568254965 <568254965@qq.com>
2019-07-18 18:15:27 +0800

    ..

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ mvn clean package -Dmaven.test.skip=true
 docker rm -f huarui_dish
 docker rmi -f huarui_dish:test
 docker build -t huarui_dish:test .
-docker run -d -p 6821:6821 --name huarui_dish huarui_dish:test java -Dspring.profiles.active=test -jar /app/app.jar
+docker run -d -p 6822:6822 --name huarui_dish huarui_dish:test java -Dspring.profiles.active=test -jar /app/app.jar
