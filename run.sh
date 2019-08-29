e003410a098fb6a79370ce9bf7c7a5fe68b8f6aa
c568254965 <568254965@qq.com>
2019-08-29 16:22:12 +0800

    .

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ git pull
 mvn clean package -Dmaven.test.skip=true
 
 docker rm -f huarui_dish
-docker rmi -f huarui_dish:test
-docker build -t huarui_dish:test .
-docker run -d -p 6822:6822 --name huarui_dish huarui_dish:test java -Dspring.profiles.active=test -jar /app/app.jar
+docker rmi -f c568254965/huarui_dish:v1.0
+docker build -t c568254965/huarui_dish:v1.0 .
+docker run -d -p 6822:6822 --name huarui_dish c568254965/huarui_dish:v1.0 java -Dspring.profiles.active=test -jar /app/app.jar
